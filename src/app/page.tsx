6551c08e1203cbd6048a1a6e286d00f94b21291d
IlyaT21 <ilijatoskovic@gmail.com>
2025-04-17 21:06:27 +0200

    posts metadata read

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,38 @@
 import fs from "fs";
 import Link from "next/link";
+import matter from "gray-matter";
+import { PostMetadata } from "@/components/PostMetadata";
 
-const getPostMetadata = () => {
+const getPostMetadata = (): PostMetadata[] => {
   const folder = "posts/";
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
-  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
-  return slugs;
+
+  const posts = markdownPosts.map((fileName) => {
+    const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8");
+    const matterResult = matter(fileContents);
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      subtitle: matterResult.data.subtitle,
+      slug: fileName.replace(".md", ""),
+    };
+  });
+
+  return posts;
 };
 
 export default function Home() {
   const postMetadata = getPostMetadata();
-  const postPreviews = postMetadata.map((slug) => (
-    <div key={slug}>
-      <Link href={`/posts/${slug}`}>
+  const postPreviews = postMetadata.map((post) => (
+    <div key={post.slug}>
+      <Link href={`/posts/${post.slug}`}>
         <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-          {slug}
+          {post.title}
         </h3>
       </Link>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">{post.subtitle}</p>
+      <p className="text-xl text-muted-foreground">{post.date}.</p>
     </div>
   ));
 
@@ -26,7 +41,7 @@ export default function Home() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Supercool Blog
       </h1>
-      <div className="flex flex-col items-center gap-4">{postPreviews}</div>
+      <div className="flex flex-col items-start gap-4">{postPreviews}</div>
     </div>
   );
 }
